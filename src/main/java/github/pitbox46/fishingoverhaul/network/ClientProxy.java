817d99602155ce45b15c77ba44f0ddd655524557
Cnuuyy <egor.su.8@gmail.com>
2025-08-29 20:23:05 +0300

    awawawawawawa

--- src/main/java/github/pitbox46/fishingoverhaul/network/ClientProxy.java
+++ src/main/java/github/pitbox46/fishingoverhaul/network/ClientProxy.java
@@ -1,6 +1,8 @@
 package github.pitbox46.fishingoverhaul.network;
 
-import github.pitbox46.fishingoverhaul.MinigameScreen;
+//import github.pitbox46.fishingoverhaul.MinigameScreen;
+import github.pitbox46.fishingoverhaul.network.MinigameResultPacket;
+import github.pitbox46.fishingoverhaul.network.PacketHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 import net.minecraftforge.common.MinecraftForge;
@@ -14,11 +16,12 @@ public class ClientProxy extends CommonProxy {
 
     @Override
     public void handleOpenMinigame(NetworkEvent.Context ctx, float catchChance, float critChance, float speedMulti) {
-        Minecraft.getInstance().setScreen(new MinigameScreen(
-                Component.translatable("screen.fishingoverhaul.minigame"),
-                catchChance,
-                critChance,
-                speedMulti
-        ));
+        //Minecraft.getInstance().setScreen(new MinigameScreen(
+        //        Component.translatable("screen.fishingoverhaul.minigame"),
+        //        catchChance,
+        //        critChance,
+        //        speedMulti
+        //));
+        PacketHandler.CHANNEL.sendToServer(new MinigameResultPacket(MinigameResultPacket.Result.CRIT));
     }
 }
